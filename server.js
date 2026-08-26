01a40921ae49a9a479c5ef0698475c2d9023b39a
amraly2226-cmyk <amr.aly.2226@gmail.com>
2026-08-26 20:50:33 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-console.log("=== SERVER START ===");
-const http = require('http');
-const port = process.env.PORT || 3000;
-http.createServer((req,res)=>{
-  res.writeHead(200);
-  res.end("Bot is running");
-}).listen(port, ()=> console.log("Listening on "+port));
-
-setInterval(()=> console.log("شغال... " + new Date().toLocaleTimeString()), 5000);
