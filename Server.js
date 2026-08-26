65586a5d804ba3fd5f83973df07b38728417b67d
amraly2226-cmyk <amr.aly.2226@gmail.com>
2026-08-26 19:59:38 +0300

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,5 @@
+   const express = require('express');
+   const app = express();
+   const PORT = process.env.PORT || 3000;
+   app.get('/', (req,res)=>res.send('PD-Bot LIVE - No crash!'));
+   app.listen(PORT, ()=>console.log('✅ Server started on ' + PORT));
